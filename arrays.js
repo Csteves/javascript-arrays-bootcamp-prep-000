f7d4b5470c634fd99530576787ba0e267d49e5ec
craig stevens <c_stevens44@hotmail.com>
2018-01-21 06:18:15 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,28 @@
+
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+var mystring = "foo"
+
+function addElementToBeginningOfArray(chocolateBars, mystring)   {
+  return [mystring,...chocolateBars]
+}
+
+function destructivelyAddElementToBeginningOfArray(chocolateBars, mystring) {
+chocolateBars.unshift(mystring);
+return chocolateBars
+}
+
+function addElementToEndOfArray(chocolateBars, mystring) {
+  return [...chocolateBars,mystring]
+}
+
+function destructivelyAddElementToEndOfArray(chocolateBars, mystring) {
+  chocolateBars.push(mystring)
+  return chocolateBars
+}
+
+function accessElementInArray(array,index) {
+  return array[index];
+}
+
+
+   
